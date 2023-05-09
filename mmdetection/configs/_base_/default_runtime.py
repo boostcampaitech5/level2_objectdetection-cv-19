ed91655b63ea6f33c7bcb328fd79e0139ff106ab
Jeon-jisu <wltnjeon0119@gmail.com>
2023-05-09 16:12:06 +0000

    feat: add MMDetWandbHook

--- mmdetection/configs/_base_/default_runtime.py
+++ mmdetection/configs/_base_/default_runtime.py
@@ -3,29 +3,37 @@ checkpoint_config = dict(interval=1)
 log_config = dict(
     interval=50,
     hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='WandbLoggerHook',interval=1000,
-            init_kwargs=dict(
-                project='vip_cv19',
-                entity = 'ENTITY 이름',
-                name = '실험할때마다 RUN에 찍히는 이름'
-            ),
-            )
+        dict(type="TextLoggerHook"),
+        dict(
+            type="MMDetWandbHook",
+            interval=100,
+            init_kwargs=dict(project="vip_cv19", name="test1"),
+            by_epoch=False,
+            num_eval_images=100,
+            # 평가에 사용된 총 이미지 수입니다.값이 0이면 validation_data_path에 있는 모든 이미지가 평가에 사용됩니다.
+            log_checkpoint=True,
+            log_checkpoint_metadata=True,
+            # config=vars(args),
+            # TODO: wandb logging 할때 config.json에서 name 가져와서 넣어줘야함
+            # 여기서 init_kwargs는 wandb.init에 전달된다.
+        )
         # dict(type='TensorboardLoggerHook')
-    ])
-# yapf:enable
-custom_hooks = [dict(type='NumClassCheckHook')]
+    ],
+)
 
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
+# yapf:enable
+custom_hooks = [dict(type="NumClassCheckHook")]
+
+dist_params = dict(backend="nccl")
+log_level = "INFO"
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
+workflow = [("train", 1)]
 
 # disable opencv multithreading to avoid system being overloaded
 opencv_num_threads = 0
 # set multi-process start method as `fork` to speed up the training
-mp_start_method = 'fork'
+mp_start_method = "fork"
 
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
