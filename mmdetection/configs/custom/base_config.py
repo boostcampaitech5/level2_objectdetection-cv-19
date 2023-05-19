a17a5c504011a2a1860e6bb025ca4a38cb9101f6
adam1206 <96027304@naver.com>
2023-05-19 07:55:25 +0000

    chore: delete unused files

--- mmdetection/configs/custom/base_config.py
+++ mmdetection/configs/custom/base_config.py
@@ -1,27 +1,29 @@
 import os
-
 from mmdet.utils import get_device
 
 _base_ = [
     "../../_base_/models/faster_rcnn_r50_fpn.py",
     "../../_base_/datasets/coco_trash_detection.py",
-    "../../_base_/schedules/schedule_1x.py",
+    "../../_base_/schedules/schedule_2x.py",
     "../../_base_/default_trash_runtime.py",
 ]
 # schedule_adamw_cosine
 
-exp_name = "MM_baseline_train30"
-weight_dir = None
-worker = "seoin"
+exp_name = "MM_baseline_fine_tuning"
+worker = "jisu"
+
+# Set folder path if resume with custom pre-trained weights
+weight_name = "MM_baseline_train40_res1024"
+weight_dir = os.path.join("/opt/ml/output/", weight_name)
 
 batch_size = 4
-max_epochs = 2
+max_epochs = 24
 device = get_device()
 
 work_dir = os.path.join("/opt/ml/output/", exp_name)
 os.makedirs(work_dir, exist_ok=True)
 
-train_annotation = "clean_30_train_fold1.json"
+train_annotation = "clean_40_train_fold1.json"
 val_annotation = "val_fold1.json"
 
 model=dict(
@@ -30,10 +32,6 @@ model=dict(
             num_classes=10)
         )
     )
-
-# optimizer_config = dict(
-#     grad_clip=dict(max_norm=35, norm_type=2)
-#     )
 
 log_config = dict(
     interval=50,
@@ -51,4 +49,4 @@ log_config = dict(
     ],
 )
 
-evaluation = dict(interval=1, save_best='bbox_mAP_50', metric='bbox') # bbox_mAP_50 기준 save
+evaluation = dict(interval=1, save_best='bbox_mAP_50')
